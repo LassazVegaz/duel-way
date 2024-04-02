f5ca7a129a1f0fb919e7556ba571cd91900dcf52
LassazVegaz <lasindunuwanga@gmail.com>
2024-04-02 10:09:37 +0530

    backend

---
+++ backend/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DuelWay.Hubs;
+
+public class ChatHub : Hub
+{
+    public const string HUB_URL = "/chat";
+
+
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,29 @@
+using DuelWay.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("dev policy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
 
 var app = builder.Build();
 
+app.UseCors("dev policy");
+
 app.MapControllers();
+
+app.MapHub<ChatHub>(ChatHub.HUB_URL);
 
 app.Run();
