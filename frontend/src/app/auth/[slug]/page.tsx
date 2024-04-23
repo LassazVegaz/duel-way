c06a265423d061d25f0129dd47f6dcb2da98ddbf
LassazVegaz <lasindunuwanga@gmail.com>
2024-04-23 11:46:42 +0530

    login check

--- frontend/src/app/auth/[slug]/page.tsx
+++ frontend/src/app/auth/[slug]/page.tsx
@@ -2,10 +2,13 @@ import PageParams from "@/types/page-params.type";
 import { redirect, RedirectType } from "next/navigation";
 import React from "react";
 import ClientComponent from "./components/ClientComponent";
+import { isLoggedIn } from "@/lib/auth.server";
 
 const ALLOWED_SLUGS = ["login", "register"];
 
 export default function Home({ params }: PageParams<{ slug: string }>) {
+  if (isLoggedIn()) redirect("/", RedirectType.replace);
+
   const slug = params.slug.toLowerCase();
   if (!ALLOWED_SLUGS.includes(slug)) {
     return redirect(`/auth/${ALLOWED_SLUGS[0]}`, RedirectType.replace);
